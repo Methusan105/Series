1a42cf526e42f88246b2e1f793c3823f3408bf11
Methusan105 <methusan@hotmail.no>
2024-04-07 15:19:39 +0200

    1212

--- upload asset files.py
+++ upload asset files.py
@@ -1,13 +1,13 @@
 import os
 
-release_file_path = r"C:\Users\Methusan\Downloads\Teen Wolf (2011) Season 03 S03 (1080p BluRay x265 HEVC 10bit DTS 5.1 Qman) [UTR]\Converted\Teen.Wolf.S03E"
+release_file_path = r"C:\Users\Methu\Downloads\Teen Wolf (2011) Season 04 S04 (1080p BluRay x265 HEVC 10bit DTS 5.1 Qman) [UTR]\Converted\Teen.Wolf.S04E"
 
 # Assuming you want to iterate from 2 to 29
-for i in range(1, 25):
+for i in range(4, 10):
     file_number = f"{i:02d}"  # Format the number with leading zeros
-    file_path = f'{release_file_path}{file_number}.mkv'
+    file_path = f'{release_file_path}{file_number}.mp4'
     
-    command = f'gh release upload TW3 "{file_path}"'
+    command = f'gh release upload TW4 "{file_path}" --clobber'
     
     # Execute the command
     os.system(command)
