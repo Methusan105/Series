b76eac88880299f0261405562e5cc7c3007d8d24
Methusan105 <methusan@hotmail.no>
2024-05-03 14:22:15 +0200

    12121212

--- upload asset files.py
+++ upload asset files.py
@@ -1,13 +1,13 @@
 import os
 
-release_file_path = r"C:\Users\Methu\Videos\Teen.Wolf.S05E"
+release_file_path = r"C:\Users\Methu\Videos\Knuckles.S01E"
 
 # Assuming you want to iterate from 2 to 29
-for i in range(19, 21):
+for i in range(1, 3):
     file_number = f"{i:02d}"  # Format the number with leading zeros
     file_path = f'{release_file_path}{file_number}.mp4'
     
-    command = f'gh release upload TW5 "{file_path}" --clobber'
+    command = f'gh release upload K1 "{file_path}" --clobber'
     
     # Execute the command
     os.system(command)
