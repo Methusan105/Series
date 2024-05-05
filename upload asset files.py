e57614ba26a53e0ca7a8392d2ec565447630a793
Methusan105 <methusan@hotmail.no>
2024-05-05 11:45:09 +0200

    121211212121212

--- upload asset files.py
+++ upload asset files.py
@@ -7,7 +7,7 @@ for i in range(6, 7):
     file_number = f"{i:02d}"  # Format the number with leading zeros
     file_path = f'{release_file_path}{file_number}.mp4'
     
-    command = f'gh release upload K1 "{file_path}" --clobber'
+    command = f'gh release upload L1 "{file_path}" --clobber'
     
     # Execute the command
     os.system(command)
