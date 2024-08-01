1fc0f10252d92d421100528521a0c944aa67182c
Methusan105 <methusan@hotmail.no>
2024-08-01 10:33:10 +0200

    1212

--- upload asset files.py
+++ upload asset files.py
@@ -1,13 +1,13 @@
 import os
 
-release_file_path = r"C:\Downloads\Stranger Things Season 3\Stranger.Things.S03E"
+release_file_path = r"C:\Downloads\2160p\Converted\Jujutsu.Kaisen.S02E"
 
 # Assuming you want to iterate from 2 to 29
-for i in range(1, 9):
+for i in range(1, 13):
     file_number = f"{i:02d}"  # Format the number with leading zeros
-    file_path = f'{release_file_path}{file_number}.mkvgh'
+    file_path = f'{release_file_path}{file_number}.mp4'
     
-    command = f'gh release upload ST3 "{file_path}" --clobber'
+    command = f'gh release upload JK2 "{file_path}" --clobber'
     
     # Execute the command
     os.system(command)
