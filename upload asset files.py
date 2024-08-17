ab76933dc7d8fc87c0720ee41448fcd0c1ce95d8
Methusan105 <methusan@hotmail.no>
2024-08-17 12:29:03 +0200

    1212

--- upload asset files.py
+++ upload asset files.py
@@ -1,13 +1,14 @@
 import os
-
-release_file_path = r"C:\Downloads\2160p\Converted\Jujutsu.Kaisen.S02E"
-
+filepath = input("Enter file Path: ")
+release_file_path = rf"{filepath}"
+release_tag = str(input("Enter Release Tag: "))
+files=int(input("How many Episodes: "))
 # Assuming you want to iterate from 2 to 29
-for i in range(1, 13):
+for i in range(1, files+1):
     file_number = f"{i:02d}"  # Format the number with leading zeros
     file_path = f'{release_file_path}{file_number}.mp4'
     
-    command = f'gh release upload JK2 "{file_path}" --clobber'
+    command = f'gh release upload {release_tag} "{file_path}" --clobber'
     
     # Execute the command
     os.system(command)
