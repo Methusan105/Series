e444533c9ffb819b5919898e25a8818711fc9832
Methusan105 <methusan@hotmail.no>
2024-08-19 12:30:30 +0200

    1212

--- upload asset files.py
+++ upload asset files.py
@@ -3,8 +3,9 @@ filepath = input("Enter file Path: ")
 release_file_path = rf"{filepath}"
 release_tag = str(input("Enter Release Tag: "))
 files=int(input("How many Episodes: "))
+startnum=int(input("Where to start: "))
 # Assuming you want to iterate from 2 to 29
-for i in range(1, files+1):
+for i in range(startnum, files+1):
     file_number = f"{i:02d}"  # Format the number with leading zeros
     file_path = f'{release_file_path}{file_number}.mp4'
     
